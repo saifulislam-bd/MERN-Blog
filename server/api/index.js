66e713d3299c85bf0f3ea2efcb8d01409c9b1616
saifulislam-bd <saifulislam.github@gmail.com>
2024-02-15 23:04:53 +0600

    database connection with dotenv

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+// database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log("DB is not connected", err.message);
+  });
+
+//server listen
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 });
