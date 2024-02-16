d6054b37bf3f37c23817295484c079f997d4f74f
saifulislam-bd <saifulislam.github@gmail.com>
2024-02-16 19:50:41 +0600

    aaded errorHandler middleware

--- server/api/index.js
+++ server/api/index.js
@@ -22,6 +22,16 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error!";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 //server listen
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if ((!username, !email, !password)) {
-    res.status(400).json({ message: "All field are required!" });
+    next(errorHandler(400, "All field are required!"));
   }
   //hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,6 +15,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully!", newUser });
   } catch (error) {
-    res.status(500).json({ message: "An error occurred!" });
+    next(error);
   }
 };
