2cbff7cfdaab8e52c3f69f6fe3776dc36ea0ef2e
saifulislam-bd <saifulislam.github@gmail.com>
2024-02-17 19:49:58 +0600

    added sign in route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if ((!username, !email, !password)) {
-    next(errorHandler(400, "All field are required!"));
+    return next(errorHandler(400, "All field are required!"));
   }
   //hash the password
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -14,6 +15,35 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json({ message: "User created successfully!", newUser });
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if ((!email, !password)) {
+    next(errorHandler(400, "All field are required!"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+
+    //compare the hashed password
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid credential!"));
+    }
+    //generate token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    const { password: pass, ...restData } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(restData);
   } catch (error) {
     next(error);
   }
