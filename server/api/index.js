6a70c8f7e3ffead1b8e7efc5df38c9014b002eb6
saifulislam-bd <saifulislam.github@gmail.com>
2024-02-20 21:38:35 +0600

    updates user API route

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRoutes from "../routes/user.route.js";
 import authRoutes from "../routes/auth.route.js";
@@ -21,6 +22,7 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
--- server/models/User.model.js
+++ server/models/User.model.js
@@ -6,13 +6,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
     },
     password: {
       type: String,
