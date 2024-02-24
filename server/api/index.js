03aa06aa82f691d22aea10f2d6b380d8730ffa9d
saifulislam-bd <saifulislam.github@gmail.com>
2024-02-24 19:54:50 +0600

    added a create post route

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import userRoutes from "../routes/user.route.js";
 import authRoutes from "../routes/auth.route.js";
+import postRoutes from "../routes/post.route.js";
 
 dotenv.config();
 const app = express();
@@ -25,6 +26,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
